603470a70f5fb1996acbb2c83d1345cfcedda93e
Passion <fabian@easy-emu.de>
2010-04-21 22:06:39 +0200

    [PreAlpha Rev 32] Fixed Acc Converter.

--- sql/Logonconverter/Old_DiamondCore_to_DiamondCore_PreAlpha_Rev30.sql
+++ sql/Logonconverter/Old_DiamondCore_to_DiamondCore_PreAlpha_Rev30.sql
@@ -2,7 +2,8 @@ ALTER TABLE account
 CHANGE COLUMN online `active_realm_id` int(11) unsigned NOT NULL DEFAULT '0';
 
 ALTER TABLE realmlist
-CHANGE COLUMN gamebuild `realmbuilds` varchar(64) NOT NULL DEFAULT '';
+CHANGE COLUMN gamebuild `realmbuilds` varchar(64) NOT NULL DEFAULT '',
+ADD COLUMN `realmflags` tinyint(3) unsigned NOT NULL DEFAULT '2' COMMENT 'Supported masks: 0x1 (invalid, not show in realm list), 0x2 (offline, set by mangosd), 0x4 (show version and build), 0x20 (new players), 0x40 (recommended)' AFTER icon;
 
 ALTER TABLE uptime
 DROP COLUMN revision;
