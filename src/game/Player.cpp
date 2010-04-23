c41f85c87b2c1ac3f54ddb242513aa636b959fce
Passion <fabian@easy-emu.de>
2010-04-23 14:59:01 +0200

    [PreAlpha Rev 39] Fixed player graveyard/homebind.

--- src/game/Player.cpp
+++ src/game/Player.cpp
@@ -4826,7 +4826,7 @@ void Player::RepopAtGraveyard()
     AreaTableEntry const *zone = GetAreaEntryByAreaID(GetAreaId());
 
     // Such zones are considered unreachable as a ghost and the player must be automatically revived
-    if ((!isAlive() && zone && zone->flags & AREA_FLAG_NEED_FLY) || GetTransport() || GetPositionZ() < -500.0f)
+    if ((!isAlive() && zone && zone->flags & AREA_FLAG_NEED_FLY) || GetTransport())
     {
         ResurrectPlayer(0.5f);
         SpawnCorpseBones();
--- src/shared/revision_nr.h
+++ src/shared/revision_nr.h
@@ -1,4 +1,4 @@
 #ifndef __REVISION_NR_H__
 #define __REVISION_NR_H__
- #define REVISION_NR "PreAlpha Rev 38"
+ #define REVISION_NR "PreAlpha Rev 39"
 #endif // __REVISION_NR_H__
